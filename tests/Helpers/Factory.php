fc8da4f67e3f551920d1ac96152047b1b4c90c38
Tom Klotz <tomklotz.pro@gmail.com>
2019-05-31 17:37:09 +0200

    style: formating style

--- tests/Helpers/Factory.php
+++ tests/Helpers/Factory.php
@@ -5,6 +5,7 @@ namespace Algolia\AlgoliaSearch\Helper\Tests\Helpers;
 final class Factory
 {
     private static $instance;
+
     /**
      * Returns a new index name based on the given `testName`.
      *
@@ -17,12 +18,12 @@ final class Factory
         if (!self::$instance) {
             self::$instance = getenv('TRAVIS') ? getenv('TRAVIS_JOB_NUMBER') : get_current_user();
         }
+
         return sprintf(
             'php_%s_%s_%s',
             date('Y-M-d_H:i:s'),
             self::$instance,
             $name
         );
-
     }
 }
