a923c240ca811b2f4ddccc44432fad2ca51b46f9
Tom Klotz <tomklotz.pro@gmail.com>
2019-05-31 17:33:50 +0200

    chore: update configuration of travis for phpunit

--- tests/Helpers/Factory.php
+++ tests/Helpers/Factory.php
@@ -4,23 +4,25 @@ namespace Algolia\AlgoliaSearch\Helper\Tests\Helpers;
 
 final class Factory
 {
+    private static $instance;
     /**
      * Returns a new index name based on the given `testName`.
      *
-     * @param string $testName
+     * @param string $name
      *
      * @return string
      */
-    public static function getIndexName($testName)
+    public static function getIndexName($name)
     {
-        $phpversion = PHP_VERSION;
+        if (!self::$instance) {
+            self::$instance = getenv('TRAVIS') ? getenv('TRAVIS_JOB_NUMBER') : get_current_user();
+        }
+        return sprintf(
+            'php_%s_%s_%s',
+            date('Y-M-d_H:i:s'),
+            self::$instance,
+            $name
+        );
 
-        date_default_timezone_set('UTC');
-
-        $date = date('Y-m-d_H:i:s');
-
-        $systemUsername = get_current_user();
-        var_dump($phpversion.'_'.$date.'_'.$systemUsername.'_'.$testName);
-        return $phpversion.'_'.$date.'_'.$systemUsername.'_'.$testName;
     }
 }
