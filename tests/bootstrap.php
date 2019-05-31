ce0770572d9507fd64f3eb687bfb4cbd9256b42c
Tom Klotz <tomklotz.pro@gmail.com>
2019-05-31 11:54:27 +0200

    fix: update key name following the CTS

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once __DIR__.'/../vendor/autoload.php';
-$envVarNames = array('ALGOLIA_APP_ID', 'ALGOLIA_API_KEY');
+$envVarNames = array('ALGOLIA_APPLICATION_ID_1', 'ALGOLIA_ADMIN_KEY_1');
 foreach ($envVarNames as $name) {
     if (!getenv($name)) {
         echo "Environment variable $name is undefined, please set one.";
