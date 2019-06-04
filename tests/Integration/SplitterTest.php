6e5071999c29a83ed4abd70d0da7763cbb83cb7d
Tom Klotz <tomklotz.pro@gmail.com>
2019-06-04 22:20:32 +0200

    tests: add more tests

--- tests/Integration/SplitterTest.php
+++ tests/Integration/SplitterTest.php
@@ -2,6 +2,7 @@
 
 namespace Algolia\AlgoliaSearch\Helper\Tests\Integration;
 
+use Algolia\AlgoliaSearch\Helper\Splitters\HtmlSplitter;
 use PHPUnit\Framework\TestCase;
 
 final class SplitterTest extends TestCase
@@ -12,8 +13,38 @@ final class SplitterTest extends TestCase
     public function testHtmlPageCanBeSplitted()
     {
         $file = file_get_contents(__DIR__.'/content/article.html');
+
         $expectedRecords = require __DIR__.'/content/article.php';
+
         $splitter = new HtmlSplitter();
-        static::assertEquals($expectedRecords, $splitter->split(null, $file));
+
+        static::assertEquals($expectedRecords, $splitter->split((string) $file));
+    }
+
+    /**
+     * @return void
+     */
+    public function testHtmlPageWithNoStandardCanBeSplitted()
+    {
+        $file = file_get_contents(__DIR__.'/content/article2.html');
+        $expectedRecords = require __DIR__.'/content/article2.php';
+        $splitter = new HtmlSplitter();
+        static::assertEquals($expectedRecords, $splitter->split((string) $file));
+    }
+
+    /**
+     * @return void
+     */
+    public function testHtmlPageWithWrongCloseTagCanBeSplitted()
+    {
+        $content = implode('', [
+            '<h1>Hello Foo!</h2>',
+        ]);
+
+        $expectedRecords = [['h1' => 'Hello Foo!', 'importance' => 0]];
+
+        $splitter = new HtmlSplitter();
+
+        static::assertEquals($expectedRecords, $splitter->split($content));
     }
 }
