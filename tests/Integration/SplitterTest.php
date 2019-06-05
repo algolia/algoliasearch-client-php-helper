e97e385ba4901f3288e3ba4fc6505c8dbb3b65ae
Tom Klotz <tomklotz.pro@gmail.com>
2019-06-05 14:17:32 +0200

    test: add multiple tests

--- tests/Integration/SplitterTest.php
+++ tests/Integration/SplitterTest.php
@@ -47,4 +47,77 @@ final class SplitterTest extends TestCase
 
         static::assertEquals($expectedRecords, $splitter->split($content));
     }
+
+    /**
+     * @return void
+     */
+    public function testHtmlPageWithDifferentParents()
+    {
+        $content = implode('', [
+            '<h1>Hello Foo!</h1>',
+            '<p>Hello Bar!</p>',
+            '<p>Hello Baz!</p>',
+        ]);
+
+        $expectedRecords = [
+            ['h1' => 'Hello Foo!', 'importance' => 0],
+            ['h1' => 'Hello Foo!', 'p' => 'Hello Bar!', 'importance' => 6],
+            ['h1' => 'Hello Foo!', 'p' => 'Hello Baz!', 'importance' => 6],
+        ];
+
+        $splitter = new HtmlSplitter();
+        static::assertEquals($expectedRecords, $splitter->split($content));
+    }
+
+    /**
+     * @return void
+     */
+    public function testHtmlPageWithSingleParagraph()
+    {
+        $content = implode('', [
+            '<p>Hello Foo!</p>',
+        ]);
+
+        $expectedRecords = [
+            ['p' => 'Hello Foo!', 'importance' => 0],
+        ];
+
+        $splitter = new HtmlSplitter();
+        static::assertEquals($expectedRecords, $splitter->split($content));
+    }
+
+    /**
+     * @return void
+     */
+    public function testHtmlPageWithMultipleParagraph()
+    {
+        $content = implode('', [
+            '<p>Hello Foo!</p>',
+            '<p>Hello Bar!</p>',
+        ]);
+
+        $expectedRecords = [
+            ['p' => 'Hello Foo!', 'importance' => 0],
+            ['p' => 'Hello Bar!', 'importance' => 0],
+        ];
+
+        $splitter = new HtmlSplitter();
+        static::assertEquals($expectedRecords, $splitter->split($content));
+    }
+
+    /**
+     * @return void
+     */
+    public function testHtmlPageWithEmptyContent()
+    {
+        $content = implode('', [
+            '',
+        ]);
+
+        $expectedRecords = [];
+
+        $splitter = new HtmlSplitter();
+
+        static::assertEquals($expectedRecords, $splitter->split($content));
+    }
 }
