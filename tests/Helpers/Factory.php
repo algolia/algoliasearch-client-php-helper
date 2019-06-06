187f94218e1f1720705e6a09dfc69434484a07f6
Tom Klotz <tomklotz.pro@gmail.com>
2019-06-06 19:53:38 +0200

    test: change indexname and restore old api id / api key name

--- tests/Helpers/Factory.php
+++ tests/Helpers/Factory.php
@@ -23,7 +23,7 @@ final class Factory
             self::$instance = getenv('TRAVIS') ? getenv('TRAVIS_JOB_NUMBER') : get_current_user();
         }
 
-        return sprintf('php-helper_%s_%s_%s', date('Y-M-d_H:i:s'), self::$instance, $name);
+        return sprintf('TRAVIS_php-helper_%s_%s_%s', date('Y-M-d_H:i:s'), self::$instance, $name);
     }
 
     /**
@@ -35,7 +35,7 @@ final class Factory
      */
     public static function getClient()
     {
-        return SearchClient::create(getenv('ALGOLIA_APP_ID'), getenv('ALGOLIA_API_KEY'));
+        return SearchClient::create(getenv('ALGOLIA_APPLICATION_ID_1'), getenv('ALGOLIA_ADMIN_KEY_1'));
     }
 
     /**
